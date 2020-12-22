990e2127df424daff794e4d154256f1dc5283935
赵晓峰(深圳联友科技有限公司IS事业部研发二部花都制造业务支持领域) <ly-zxfeng@dfl.com.cn>
2020-12-22 16:48:25 +0800

    将公共配置如数据源配置、dubbo配置等等放到公共模块

--- providers/src/main/java/com/zxf/ProviderApp.java
+++ providers/src/main/java/com/zxf/ProviderApp.java
@@ -2,11 +2,15 @@ package com.zxf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
+
 //@EnableDubbo
 @SpringBootApplication
 @ImportResource({"classpath:dubbo-provider.xml"})
+@PropertySource(value={"classpath:jdbc.properties","classpath:dubbo.properties"})
 @EnableCaching
 public class ProviderApp {
 	public static void main(String[] args) {
